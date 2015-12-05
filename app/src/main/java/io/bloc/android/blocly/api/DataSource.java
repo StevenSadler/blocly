a2a6b13e3c7f9be77d60caceaed407f28b3c6f12
Steven Sadler <stevenjsadler@gmail.com>
2015-12-05 11:26:18 -0800

    assignment 38 complete

--- app/src/main/java/io/bloc/android/blocly/api/DataSource.java
+++ app/src/main/java/io/bloc/android/blocly/api/DataSource.java
@@ -13,6 +13,7 @@ public class DataSource {
 
     private List<RssFeed> feeds;
     private List<RssItem> items;
+    private String backgroundImageUrl;
 
     public DataSource() {
         feeds = new ArrayList<RssFeed>();
@@ -28,6 +29,10 @@ public class DataSource {
         return items;
     }
 
+    public String getBackgroundImageUrl() {
+        return backgroundImageUrl;
+    }
+
     void createFakeData() {
         feeds.add(new RssFeed("My Favorite Feed",
                 "This feed is just incredible, I can't even begin to tell you...",
@@ -38,8 +43,10 @@ public class DataSource {
                     "An incredible news story #" + i,
                     "You won't believe how exciting this news story is, get ready to be blown away by its amazingness.",
                     "http://favoritefeed.net?story_id=an-incredible-news-story",
-                    "http://rs1img.memecdn.com/silly-dog_o_511213.jpg",
+                    "https://upload.wikimedia.org/wikipedia/en/5/56/Loopy_Funny_Dog.jpg",
                     0, System.currentTimeMillis(), false, false, false));
         }
+        //backgroundImageUrl = "http://m5.paperblog.com/i/80/805796/the-worlds-top-10-best-images-of-dogs-wearing-L-6s9Zgt.jpeg";
+        backgroundImageUrl = "http://www.photos-public-domain.com/wp-content/uploads/2011/01/red-pawprint-wall-decal-190x190.jpg";
     }
 }
--- app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -1,11 +1,20 @@
 package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
 import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 
@@ -15,8 +24,9 @@ import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 public class BloclyActivity extends Activity {
 
     private ItemAdapter itemAdapter;
+    private static String TAG = BloclyActivity.class.getSimpleName();
+    private ImageView backgroundImageView;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
@@ -28,5 +38,36 @@ public class BloclyActivity extends Activity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(itemAdapter);
+
+
+
+
+        String imageUrl = BloclyApplication.getSharedDataSource().getBackgroundImageUrl();
+        backgroundImageView = (ImageView)findViewById(R.id.iv_activity_blocly_background);
+        ImageLoadingListener listener = new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                Log.e(TAG, "onLoadingFailed: " + failReason.toString() + " for URL: " + imageUri);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                backgroundImageView.setImageBitmap(loadedImage);
+                backgroundImageView.setVisibility(View.VISIBLE);
+                Log.i(TAG, "onLoadingCompleted: for URL: " + imageUri, new Throwable("Throwable test"));
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                // Attempt a retry
+                ImageLoader.getInstance().loadImage(imageUri, this);
+            }
+        };
+        ImageLoader.getInstance().loadImage(imageUrl, listener);
     }
 }
