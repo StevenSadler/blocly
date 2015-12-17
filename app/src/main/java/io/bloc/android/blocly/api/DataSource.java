b39479025a45019a7225572df473200b523b937a
Steven Sadler <stevenjsadler@gmail.com>
2015-12-17 00:58:23 -0800

    assignment 53 complete

--- app/src/main/java/io/bloc/android/blocly/api/DataSource.java
+++ app/src/main/java/io/bloc/android/blocly/api/DataSource.java
@@ -1,9 +1,14 @@
 package io.bloc.android.blocly.api;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.BuildConfig;
@@ -19,6 +24,8 @@ import io.bloc.android.blocly.api.network.GetFeedsNetworkRequest;
  * Created by Steven on 11/30/2015.
  */
 public class DataSource {
+
+    private static String TAG = DataSource.class.getSimpleName();
 
     private DatabaseOpenHelper databaseOpenHelper;
     private RssFeedTable rssFeedTable;
@@ -39,12 +46,70 @@ public class DataSource {
 
         new Thread(new Runnable() {
             @Override
-        public void run() {
+            public void run() {
                 if (BuildConfig.DEBUG && false) {
                     BloclyApplication.getSharedInstance().deleteDatabase("blocly_db");
                 }
                 SQLiteDatabase writeableDatabase = databaseOpenHelper.getWritableDatabase();
-                new GetFeedsNetworkRequest("http://feeds.feedburner.com/androidcentral?format=xml").performRequest();
+                List<GetFeedsNetworkRequest.FeedResponse> feedResponses =
+                        new GetFeedsNetworkRequest("http://feeds.feedburner.com/androidcentral?format=xml").performRequest();
+                GetFeedsNetworkRequest.FeedResponse androidCentral = feedResponses.get(0);
+
+                /*
+                 * search the feed table to see if this feed is already recorded
+                 * if it is not a dupe, add it to the table and return the feedId
+                 * if it is a dupe, return -1
+                 */
+
+                long androidCentralFeedId = new RssFeedTable.Builder()
+                        .setFeedURL(androidCentral.channelFeedURL)
+                        .setSiteURL(androidCentral.channelURL)
+                        .setTitle(androidCentral.channelTitle)
+                        .setDescription(androidCentral.channelDescription)
+                        .insert(writeableDatabase);
+
+
+                if (androidCentralFeedId != -1) {
+                    Log.i(TAG, "insert feed : " + androidCentral.channelURL);
+                } else {
+                    Log.i(TAG, "insert feed FAILED, possible duplicate : " + androidCentral.channelURL);
+                }
+
+                for (GetFeedsNetworkRequest.ItemResponse itemResponse : androidCentral.channelItems) {
+                    long itemPubDate = System.currentTimeMillis();
+                    DateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy kk:mm:ss z", Locale.ENGLISH);
+                    try {
+                        itemPubDate = dateFormat.parse(itemResponse.itemPubDate).getTime();
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    /*
+                     * search the item table to see if this item is already recorded
+                     * if it is not a dupe, add it to the table and return itemId
+                     * if it is a dupe return -1
+                     */
+
+                    long androidCentralItemId = new RssItemTable.Builder()
+                            .setTitle(itemResponse.itemTitle)
+                            .setDescription(itemResponse.itemDescription)
+                            .setEnclosure(itemResponse.itemEnclosureURL)
+                            .setMIMEType(itemResponse.itemEnclosureMIMEType)
+                            .setLink(itemResponse.itemURL)
+                            .setGUID(itemResponse.itemGUID)
+                            .setPubDate(itemPubDate)
+
+                            .setRSSFeed(androidCentralFeedId)
+                            .insert(writeableDatabase);
+
+
+                    if (androidCentralItemId != -1) {
+                        Log.i(TAG, "insert item : " + itemResponse.itemGUID);
+                    } else {
+                        Log.i(TAG, "insert item FAILED, possible duplicate : " + itemResponse.itemGUID);
+                    }
+
+                }
             }
         }).start();
     }
